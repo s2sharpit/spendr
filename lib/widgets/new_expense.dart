d369c56585501c8ef848fa124ab8dec81e877246
Tushar Saini <s2sharpit@gmail.com>
2025-04-01 17:42:05 +0530

    142 building adaptive showdialog

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spendr/models/expense.dart';
 
@@ -34,12 +37,25 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
-  void _submitExpenseData() {
-    final enteredAmt = double.tryParse(_amountController.text);
-    final amtIsInvalid = enteredAmt == null || enteredAmt <= 0;
-    if (_titleController.text.trim().isEmpty ||
-        amtIsInvalid ||
-        _selectedDate == null) {
+  void _showDialog() {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (ctx) => CupertinoAlertDialog(
+          title: const Text('Invalid Input'),
+          content: const Text(
+              'Please make sure a valid title, amount, date and category was entered.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
+            )
+          ],
+        ),
+      );
+    } else {
       showDialog(
         context: context,
         builder: (ctx) => AlertDialog(
@@ -56,6 +72,16 @@ class _NewExpenseState extends State<NewExpense> {
           ],
         ),
       );
+    }
+  }
+
+  void _submitExpenseData() {
+    final enteredAmt = double.tryParse(_amountController.text);
+    final amtIsInvalid = enteredAmt == null || enteredAmt <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amtIsInvalid ||
+        _selectedDate == null) {
+      _showDialog();
       return;
     }
 

98d64b2cadccefd8aeef9ebd316e89d6af82b302
Tushar Saini <s2sharpit@gmail.com>
2025-04-01 16:46:43 +0530

    making home slide responsive

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -67,6 +67,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expense found. Start adding some!'),
     );
@@ -89,12 +91,21 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(child: mainContent),
+              ],
+            ),
     );
   }
 }
